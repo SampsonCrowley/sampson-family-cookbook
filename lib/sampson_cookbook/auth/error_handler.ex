c123ae70c69db04db503c75136ca12088a108bb8
Sampson Crowley <sampsonsprojects@gmail.com>
2019-12-25 19:28:11 -0700

    remove unused type variable in auth error handler

--- lib/sampson_cookbook/auth/error_handler.ex
+++ lib/sampson_cookbook/auth/error_handler.ex
@@ -4,7 +4,7 @@ defmodule SampsonCookbook.Auth.ErrorHandler do
   @behaviour Guardian.Plug.ErrorHandler
 
   @impl Guardian.Plug.ErrorHandler
-  def auth_error(conn, {type, _reason}, _opts) do
+  def auth_error(conn, {_type, _reason}, _opts) do
     conn
     |> put_flash(:error, "You must be signed in to access this page")
     |> redirect(to: Routes.session_path(conn, :new))
